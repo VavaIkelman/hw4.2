c4b343a27098a4713417ccb40ee0d7b79acb2a4c
Вячеслав Илькевич <ivada@MacBook-Pro-Vaceslav.local>
2021-03-28 20:46:52 +0300

    Eslint done

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,33 +1,33 @@
 import sortHealth from '../app';
 
 test('sort toEqual', () => {
-    const input = [
-        {name: 'мечник', health: 10},
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80}, 
-    ];
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
 
-    const expected = [
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80}, 
-        {name: 'мечник', health: 10},
-    ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
 
-    expect(sortHealth(input)).toEqual(expected);
+  expect(sortHealth(input)).toEqual(expected);
 });
 
 test('sort not.toBe', () => {
-    const input = [
-        {name: 'мечник', health: 10},
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80}, 
-    ];
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
 
-    const expected = [
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80}, 
-        {name: 'мечник', health: 10},
-    ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
 
-    expect(sortHealth(input)).not.toBe(expected);
+  expect(sortHealth(input)).not.toBe(expected);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 export default function sortHealth(arr) {
-    return arr.sort((a, b) => b.health - a.health);
+  return arr.sort((a, b) => b.health - a.health);
 }
